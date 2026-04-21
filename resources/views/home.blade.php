764c82501906a4bc9de2231a07a03f80e9ea7ef8
lucariggi92 <riggiluca92@gmail.com>
2026-04-21 18:38:59 +0200

    feat: table completed

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,9 +2,40 @@
 
 
 @section("tabella")
-<ul>
-    @foreach($trains as $train)
-    <li>{{$train}}</li>
-    @endforeach
-</ul>
+
+<table>
+    <thead>
+        <tr>
+            <th>Azienda</th>
+            <th>Codice Treno</th>
+            <th>Stazione di arrivo</th>
+            <th>Stazione di partenza</th>
+            <th>Orario di partenza</th>
+            <th>Orario di arrivo</th>
+            <th>Totale Carrozze</th>
+            <th>Sei in orario o meno</th>
+            <th>Se cancellato o meno</th>
+        </tr>
+    </thead>
+    <tbody>
+         @foreach($trains as $train)
+        <tr>
+           
+            <td>{{$train->company}}</td>
+            <td>{{$train->train_code}}</td>
+            <td>{{$train->departure_station}}</td>
+            <td>{{$train->arrival_station}}</td>
+            <td>{{$train->departure_time}}</td>
+            <td>{{$train->arrival_time}}</td>
+            <td>{{$train->total_carriages}}</td>
+            <td>{{ $train->is_on_time ? 'Sì' : 'No' }}</td>
+            <td>{{ $train->is_cancelled ? 'Sì' : 'No' }}</td>
+          
+        </tr>
+           @endforeach
+    </tbody>
+</table>
+
+    
+
 @endsection
